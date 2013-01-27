92f021b0770cf7504bbe5df17d89044b97fc901b
vub <vub@gmail.com>
2013-01-27 13:00:20 -0500

    Changed algorithm to recursive search because a full traversal is needed anyway to rule out mixing, and added more complex test case transactions.

--- gettxcolor.py
+++ gettxcolor.py
@@ -3,7 +3,6 @@ import os
 import json
 import connector
 from armoryengine import *
-from heapq import *
 import urllib2
 from colortools import *
 
@@ -11,7 +10,7 @@ BDM_LoadBlockchainFile()
 
 DEBUG = False
 
-def get_parent_inputs (pytx, index):
+def get_parent_inputs (pytx, index,indent=0):
     inputs = []
     for i,inp in enumerate(pytx.inputs):
         txhash = inp.outpoint.txHash
@@ -25,44 +24,58 @@ def get_parent_inputs (pytx, index):
     outputs = []
     for outpt in pytx.outputs:
         outputs.append([outpt.value, 0])
+    # A list of preceding sums by input and output
+    PSI = reduce(lambda x,y: x + [x[-1]+y[0]], [[0]]+inputs)
+    PSO = reduce(lambda x,y: x + [x[-1]+y[0]], [[0]]+outputs)
+    
+    if DEBUG:
+        print "   " * indent, inputs, PSI, " | " , outputs, PSO, " (index = %d" % index
 
-    compute_colors(inputs,outputs)
+    # Get all inputs that flow into the given output
     parents = []
     for i,inp in enumerate(inputs):
-      if inp[1] == outputs[index][1]: parents.append(i)
+        if PSI[i] < PSO[index+1] and PSI[i+1] > PSO[index]: 
+            parents.append(i)
     return parents
 
 def hextobin(x): return hex_to_binary(x,endIn=LITTLEENDIAN, endOut=BIGENDIAN)
 def bintohex(x): return binary_to_hex(x,endIn=LITTLEENDIAN, endOut=BIGENDIAN)
 
-def search_for_color (txhash,index):
+def search_for_color_binhash (txhash,index,issues,indent=0):
+
+
+    if DEBUG: 
+        print "   " * indent + bintohex(txhash), "Index: " + str(index)
+
+    mytx = TheBDM.getTxByHash(txhash)
+    # Is coinbase, return uncolored
+    if mytx.getTxIn(0).isCoinbase(): return None
+
+    # Matches color issue, return color
+    hexhash = bintohex(txhash)
+    if hexhash+str(index) in issues: 
+        return issues[hexhash+str(index)]
+    
+    # Get parent inputs
+    parents = get_parent_inputs(PyTx().unserialize(mytx.serialize()),index,indent)
+
+    pc = []
+    for p in parents:
+      # Get parent tx
+      txp_outpoint = mytx.getTxIn(p).getOutPoint()
+
+
+      # Recursive search is required since we need to make sure that ALL parent inputs are of the same color
+      # for the output to be colored (any mixing leads to uncolored coins)
+      pc.append(search_for_color_binhash(txp_outpoint.getTxHash(),txp_outpoint.getTxOutIndex(),issues,indent+1))
+      if pc[-1] == None: return None
+      if len(pc) > 1 and pc[-1] != pc[-2]: return None
+    if len(pc) == 0: return None
+    return pc[0]
+
+def search_for_color(txhash, inp):
     issues = {}
     for cdef in color_definitions:
         for i in cdef[1]["issues"]:
             issues[i["txhash"]+str(i["outindex"])] = cdef[0]
-    heap = []
-
-    mytx = TheBDM.getTxByHash(hextobin(txhash))
-    if mytx.getTxIn(0).isCoinbase(): return None
-
-    heappush(heap,[0,mytx,index])
-
-    while len(heap) > 0:
-        top = heappop(heap)
-        # Matches color issue, return color
-        hexhash = bintohex(top[1].getThisHash())
-        if hexhash+str(top[2]) in issues: 
-            return issues[hexhash+str(top[2])]
-        # Get parent tx
-        txp_outpoint = top[1].getTxIn(top[2]).getOutPoint()
-        txparent = TheBDM.getTxByHash(txp_outpoint.getTxHash())
-        # Is parent coinbase, return -1
-        if txparent.getTxIn(0).isCoinbase():
-            return -1
-        if DEBUG: 
-          print bintohex(txp_outpoint.getTxHash()),txp_outpoint.getTxOutIndex()
-        for p in get_parent_inputs(PyTx().unserialize(txparent.serialize()),txp_outpoint.getTxOutIndex()):
-            # Modify the top[0] + 1 to make a different search algorithm
-            heappush(heap,[top[0]+1,txparent,p])
-    return None
-
+    return search_for_color_binhash(hextobin(txhash),inp,issues)
--- txcolortest.py
+++ txcolortest.py
@@ -1,8 +1,13 @@
-from gettxcolor import *
+import gettxcolor as g
 
 # Substitute in your own transactions and expected colors
 
-print search_for_color('3a60b70d425405f3e45f9ed93c30ca62b2a97e692f305836af38a524997dd01d',0), "== None"
-print search_for_color('c1d8d2fb75da30b7b61e109e70599c0187906e7610fe6b12c58eecc3062d1da5',0), "== Red"
-print search_for_color('8f6c8751f39357cd42af97a67301127d497597ae699ad0670b4f649bd9e39abf',0), "== Red"
-print search_for_color('f50f29906ce306be3fc06df74cc6a4ee151053c2621af8f449b9f62d86cf0647',0), "== Blue"
+#g.DEBUG = True
+
+print g.search_for_color('3a60b70d425405f3e45f9ed93c30ca62b2a97e692f305836af38a524997dd01d',0), "== None (Random TX from blockchain)"
+print g.search_for_color('c1d8d2fb75da30b7b61e109e70599c0187906e7610fe6b12c58eecc3062d1da5',0), "== Red"
+print g.search_for_color('8f6c8751f39357cd42af97a67301127d497597ae699ad0670b4f649bd9e39abf',0), "== Red"
+print g.search_for_color('f50f29906ce306be3fc06df74cc6a4ee151053c2621af8f449b9f62d86cf0647',0), "== Blue"
+print g.search_for_color('7e40d2f414558be60481cbb976e78f2589bc6a9f04f38836c18ed3d10510dce5',0), "== Blue"
+print g.search_for_color('4b60bb49734d6e26d798d685f76a409a5360aeddfddcb48102a7c7ec07243498',0), "== Red (Two-input merging TX)"
+print g.search_for_color('342f119db7f9989f594d0f27e37bb5d652a3093f170de928b9ab7eed410f0bd1',0), "== None (Color mixing TX)"
